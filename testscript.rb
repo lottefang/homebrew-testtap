a8a185dc7eed14e313c0d09ce4f74aa41e9e1c77
lottefang <lottefang@126.com>
2021-07-18 05:45:49 +0800

    add testscript.rb

---
+++ testscript.rb
@@ -0,0 +1,13 @@
+class Testscript < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/lottefang/testscript/archive/refs/tags/foobar.tar.gz"
+  version ""
+  sha256 ""
+  license ""
+
+
+  def install
+	bin.install "testscript"
+  end
+end
