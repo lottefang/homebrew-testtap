69889c31cf2911d2315b41505ac316c7eacbdc10
lottefang <lottefang@126.com>
2021-07-18 05:48:11 +0800

    add versionnumber

--- testscript.rb
+++ testscript.rb
@@ -2,7 +2,7 @@ class Testscript < Formula
   desc ""
   homepage ""
   url "https://github.com/lottefang/testscript/archive/refs/tags/foobar.tar.gz"
-  version ""
+  version "1.0.0"
   sha256 ""
   license ""
 
